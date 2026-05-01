cf12fb906ffc801154bf407d80ddf797abd2ee05
Sathy <sathyamech1357@gamil.com>
2026-05-01 19:17:54 +0530

    dotnet runner added

--- learningCsharp.Core/obj/Debug/net10.0/learningCsharp.Core.AssemblyInfo.cs
+++ learningCsharp.Core/obj/Debug/net10.0/learningCsharp.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("learningCsharp.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472dcb7f1090a3ff28f28ce72fc330ebf4f5753f")]
 [assembly: System.Reflection.AssemblyProductAttribute("learningCsharp.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("learningCsharp.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
